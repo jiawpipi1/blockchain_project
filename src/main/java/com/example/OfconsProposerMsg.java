2f326e3fe90f2b85d87d11b68349d710c9a21cae
pitsaiyang <leowang46796047@gmail.com>
2025-03-18 21:53:04 +0100

    first commit

---
+++ src/main/java/com/example/OfconsProposerMsg.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class OfconsProposerMsg {
+	
+	public String message;
+	
+	public OfconsProposerMsg(String i) {
+		this.message = i;
+	}
+	public int v; // 111
+	
+	public OfconsProposerMsg(int v) {
+		this.v = v;
+	}
+
+}
